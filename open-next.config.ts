cf4cecb3f9d2996190af8c728909ee11d7a80266
Vishwajeet Guru <gawarguruvishwajeet@gmail.com>
2026-08-10 17:10:22 +0530

    add cloudflare opennext deployment config

---
+++ open-next.config.ts
@@ -0,0 +1,4 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+// See https://opennext.js.org/cloudflare/caching to enable R2 cache storage.
+export default defineCloudflareConfig();
